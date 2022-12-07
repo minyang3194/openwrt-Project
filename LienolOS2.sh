ad4078cb49b13ade535d5ae8298e06d4c4eea642
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2022-12-08 04:05:50 +0800

    Update LienolOS2.sh

--- LienolOS2.sh
+++ LienolOS2.sh
@@ -16,7 +16,7 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/luci/master/applications/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update > ./feeds/other/lean/luci-app-adbyby-plus/root/usr/share/adbyby/ad-update
 curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.99/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 rm -rf ./package/diy-ziyong/theme
 rm -rf ./package/diy-ziyong/luci-app-wrtbwmon
